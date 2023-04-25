162634fb1affed33b61f5b8637fa249f6b616fdb
Aniq Haider <aniqhaider1@gmail.com>
2023-04-25 18:43:07 +0530

    day 1 and day 2 done

---
+++ 1_odd_or_even.js
@@ -0,0 +1,14 @@
+function test(num){
+    if(num%2==0){
+        console.log('even');
+    }else{
+        console.log('odd')
+    }
+}
+
+let value = 712221;
+test(value);
+
+
+let values = 23456;
+test(values);
---
+++ 2_addTwoNumbers.js
@@ -0,0 +1,8 @@
+function addition(a,b){
+    return a + b
+};
+
+let num1 = 5;
+let num2 = 15;
+
+console.log(addition(num1, num2))
