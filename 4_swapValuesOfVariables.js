a279bddc9a2a990bc1d56dc4dfdac2b6daf79a1d
Aniq Haider <aniqhaider1@gmail.com>
2023-04-26 19:25:27 +0530

    day 4

---
+++ 4_swapValuesOfVariables.js
@@ -0,0 +1,38 @@
+// pogram to swap values of two variables --
+
+// method 1
+console.log("method 1")
+let a = 10; 
+let b = 20;
+
+let c = a;
+
+a = b;
+b = c;
+
+console.log(a)
+console.log(b)
+
+// method 2
+console.log("Method 2")
+let num1 = 19;
+let num2 = 21;
+
+num1 = num1 + num2;
+num2 = num1 - num2;
+num1 = num1 - num2;
+
+console.log(num1);
+console.log(num2);
+
+// method 3
+console.log("method 3")
+let number1 = 12;
+let number2 = 13;
+
+number1 = number1^number2
+number2 = number1^number2
+number1 = number1^number2
+
+console.log(number1);
+console.log(number2)
