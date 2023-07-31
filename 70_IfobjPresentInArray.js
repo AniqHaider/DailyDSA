ace927c7013e123b09a490d8df59d32bd26774c7
AniqHaider <aniqhaider@gmail.com>
2023-07-31 13:17:45 +0530

    day 70 addede

---
+++ 70_IfobjPresentInArray.js
@@ -0,0 +1,10 @@
+// JavaScript Program to Check if An Object is An Array
+
+const checkIfArray = (obj) => {
+  return Array.isArray(obj);
+};
+let myArray = [1, 2, 3, 4, 5];
+let myObject = { name: "John", age: 25 };
+
+console.log(checkIfArray(myArray));
+console.log(checkIfArray(myObject));
